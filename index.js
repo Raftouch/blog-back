b5bf440799b0ec6257a223751c8a71ea1033ea06
Raftouch <rafanelkin@gmail.com>
2023-05-18 12:58:48 +0200

    :tada: project setup, i dependencies

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb+srv://rafa:Uy6PES6HxKA8UTV@cluster0.oinetw9.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB works perfectly'))
+    .catch((err) => console.log('DB error occured', err))
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.post('/auth/login', (req, res) => {
+    console.log(req.body)
+
+    // when we get a request, we would need to generate a jwt
+    const token = jwt.sign({
+        name: req.body.name,
+        email: req.body.email
+    }, 'secretkey123')
+
+    res.json({ 
+        message: 'login successfull',
+        token
+    })
+})
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Server is running on port 4444')
+})
